26e82a1359d1c70b0b0f63f14de5fc0bbf8209bc
Joshua Kruzan <joshua_kruzan@brown.edu>
2022-11-07 20:19:05 -0500

    Forgotten files from last commit

---
+++ clustervis/charts/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .load_data import get_feature_names
+
+class FeaturesForm(forms.Form):
+    feature_names = get_feature_names()
+    feature_choices = [(i, choice) for i, choice in enumerate(feature_names)]
+    features_field = forms.MultipleChoiceField(label='Features', choices=feature_choices, widget=forms.CheckboxSelectMultiple)
